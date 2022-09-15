da027f94aed9b00ad884a41d50875eddc17bcdb1
musamolla3131 <musamolla3131@gmail.com>
2022-09-15 23:49:33 +0600

    Start button problem solve

--- script.js
+++ script.js
@@ -113,8 +113,8 @@ const start = () => {
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
-      display.classList.remove("inactive");
+      // countdownOverlay.style.display = "flex";
+      // display.classList.remove("inactive");
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
