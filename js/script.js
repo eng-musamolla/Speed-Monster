caf223db8ed2856ebd253e9be81607ada435c494
musamolla3131 <musamolla3131@gmail.com>
2022-09-15 23:54:58 +0600

    blog part

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ const gameOver = () => {
     <h1>Finished!</h1>
     <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
-    <button onclick="closeModal()">Close</button>
+    <button style="margin-top:30px;" onclick="closeModal()">Close</button>
   `;
 
   addHistory(questionText, timeTaken, errorCount);
